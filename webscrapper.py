eaad759df6471fa3bd9051e999fdf95d3ae3d5e5
20tk11 <tomaskanapickass@gmail.com>
2021-08-04 21:41:05 +0300

    changes and fixes

--- webscrapper.py
+++ webscrapper.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 
 class Data:
     def __init__(self, code, emp, sal, inc):
@@ -16,46 +17,61 @@ class Data:
         print("Vidutinis atlyginimas: " + self.AvgSalary)
         print("Pardavimo pajamos: " + self.SellingIncome)
 
-driver = webdriver.Chrome()
-driver.get('https://rekvizitai.vz.lt/')
-searchbox = driver.find_element_by_xpath('//*[@id="code"]')
-searchbox.send_keys('300086828')
-searchbutton = driver.find_element_by_xpath('//*[@id="ok"]')
-searchbutton.click()
-searchbutton2 = driver.find_element_by_xpath('//*[@id="leftContainer"]/div[1]/div/div[3]/div[2]/a[1]')
-searchbutton2.click()
-try:
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "info"))
-    )
-    #print(element.text)
-    rows = element.find_elements_by_tag_name("tr")
-    value1 = ""
-    value2 = ""
-    value3 = ""
-    value4 = ""
-    for x in range(len(rows)):
-        columns = rows[x].find_elements_by_tag_name("td")
-        #print(x)
-        #print(columns[1].text)
-        #print(columns[2].text)
+w = open("temp.txt", "a")
+s = open("gas.txt", "r")
+lines = s.readlines()
+print(len(lines))
+s.close()
+temp = 0
+print(len(lines))
+array = []
+for x in lines:
+    driver = webdriver.Chrome()
+    driver.get('https://rekvizitai.vz.lt/')
+    searchbox = driver.find_element_by_xpath('//*[@id="code"]')
+    searchbox.send_keys(x)
+    searchbutton = driver.find_element_by_xpath('//*[@id="ok"]')
+    searchbutton.click()
+    time.sleep(3) #If there will be more frequent queries a human authentification window pops up
+    searchbutton2 = driver.find_element_by_xpath('//*[@id="leftContainer"]/div[1]/div/div[3]/div[2]/a[1]')
+    searchbutton2.click()
+    try:
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "info"))
+        )
+        #print(element.text)
+        rows = element.find_elements_by_tag_name("tr")
+        value1 = "\n"
+        value2 = "\n"
+        value3 = "\n"
+        value4 = "\n"
+        for x in range(len(rows)):
+            columns = rows[x].find_elements_by_tag_name("td")
+            #print(x)
+            #print(columns[1].text)
+            #print(columns[2].text)
 
-        if(columns[1].text == "Įmonės kodas"):
-            value1 = columns[2].text
-        if(columns[1].text == "Darbuotojai"):
-            value2 = columns[2].text
+            if(columns[1].text == "Įmonės kodas"):
+                value1 = columns[2].text + "\n"
+            if(columns[1].text == "Darbuotojai"):
+                value2 = columns[2].text + "\n"
         
-        if(columns[1].text == "Vidutinis atlyginimas"):
-            value3 = columns[2].text
+            if(columns[1].text == "Vidutinis atlyginimas"):
+                value3 = columns[2].text + "\n"
         
-        if(columns[1].text == "Pardavimo pajamos"):
-            value4 = columns[2].text
-    h1 = Data(value1, value2, value3, value4)
-    h1.display()
-finally:
-    driver.quit()
-
-
+            if(columns[1].text == "Pardavimo pajamos"):
+                value4 = columns[2].text + "\n"
+        w.write(value1)
+        w.write(value2)
+        w.write(value3)
+        w.write(value4)
+        h1 = Data(value1, value2, value3, value4)
+        array.append(h1)
+        array[temp].display()
+        temp = temp + 1
+    finally:
+        driver.quit()
+w.close()
 #element = driver.find_element_by_class_name('info')
 
 
